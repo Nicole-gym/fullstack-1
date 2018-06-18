5d9be0cd67c65f93025639c07fa99be0f76ef579
Nicole-gym <yg2529@columbia.edu>
2018-06-18 17:33:44 -0400

    dataservice

---
+++ week1/oj-client/src/app/services/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Problem } from '../models/problem.model';
+import { PROBLEMS } from '../mock_problems';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  //list of problems
+  problems: Problem[] = PROBLEMS;
+
+  constructor() { }
+
+  getProblems(): Problem[] {
+    return this.problems;
+
+  }
+
+  getProblem(id : number): Problem {
+    return this.problems.find((problem) => problem.id === id);
+  }
+}
