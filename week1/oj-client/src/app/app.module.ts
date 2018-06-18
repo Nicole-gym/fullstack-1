6a11338ae44bc5e1a435a408a85833948aa08989
Nicole-gym <yg2529@columbia.edu>
2018-06-18 19:10:06 -0400

    routes

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { DataService } from './services/data.service';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProblemListComponent
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
     BrowserModule
