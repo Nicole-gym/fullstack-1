bf06b6ef95d12555646ffa8527dd98b7738b6b02
Nicole-gym <yg2529@columbia.edu>
2018-07-12 17:42:58 -0400

    connect mongodb

--- week2/oj-server/server.js
+++ week2/oj-server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const restRouter = require('./routes/rest');
+const mongoose = require('mongoose');
 
 // app.get('/', (req, res) => res.send('Hello World!!!!!!'));
+
 app.use('/api/v1', restRouter);
 
+
+mongoose.connect('mongodb://Nicole-gym:nicole-gym123@ds235239.mlab.com:35239/problems');
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
